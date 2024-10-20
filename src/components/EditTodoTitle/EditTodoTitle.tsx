c337a07b2964b3c8edfa086201a9e9568897a142
Dmitry <kononenko.dima2001@gmail.com>
2024-10-20 23:14:20 +0300

    solution

---
+++ src/components/EditTodoTitle/EditTodoTitle.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  handleChangeTodo: <T>(
+    callback: (id: T) => Promise<void[]>,
+    valueToCall: T,
+  ) => void;
+  todo: Todo;
+  setIsEditing: (state: boolean) => void;
+  onUpdateTodo: (todo: Todo[]) => Promise<void[]>;
+  onDeleteTodo: (id: number[]) => Promise<void[]>;
+}
+
+export const EditTodoTitle: React.FC<Props> = ({
+  onDeleteTodo,
+  handleChangeTodo,
+  todo,
+  setIsEditing,
+  onUpdateTodo,
+}) => {
+  const [titleName, setTitleName] = useState(todo.title);
+
+  const handleChangeTitle = (newTitle: string) => {
+    setTitleName(newTitle);
+  };
+
+  const inputField = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    inputField.current?.focus();
+  }, [todo]);
+
+  const checkPressedKey = (key: string) => {
+    if (key === 'Escape') {
+      handleChangeTitle(todo.title);
+      setIsEditing(false);
+    }
+  };
+
+  const handleSubmit = (event?: React.FormEvent<HTMLFormElement>) => {
+    if (event) {
+      event.preventDefault();
+    }
+
+    if (titleName === todo.title) {
+      setIsEditing(false);
+
+      return;
+    }
+
+    if (!titleName) {
+      handleChangeTodo(onDeleteTodo, [todo.id]);
+
+      return;
+    }
+
+    onUpdateTodo([
+      {
+        ...todo,
+        title: titleName.trim(),
+      },
+    ]).then(() => setIsEditing(false));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        ref={inputField}
+        data-cy="TodoTitleField"
+        type="text"
+        className="todo__title-field"
+        placeholder="Empty todo will be deleted"
+        value={titleName}
+        onChange={event => handleChangeTitle(event.target.value)}
+        onBlur={() => {
+          handleSubmit();
+        }}
+        onKeyUp={event => checkPressedKey(event.key)}
+      />
+    </form>
+  );
+};
